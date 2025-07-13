68fefdae95f8f21bfe7143d08a1bc29c807db9d5
nj.jo <jonamjun.dev@gmail.com>
2025-07-13 16:01:58 +0900

    refactor: change indexing API endpoint from /api/indexing to /api/index

--- confluence_gateway/api/routes/__init__.py
+++ confluence_gateway/api/routes/__init__.py
@@ -7,6 +7,6 @@ from .spaces import router as spaces_router
 
 api_router = APIRouter()
 api_router.include_router(search_router, prefix="/search", tags=["Search"])
-api_router.include_router(indexing_router, prefix="/indexing", tags=["Indexing"])
+api_router.include_router(indexing_router, prefix="/index", tags=["Indexing"])
 api_router.include_router(generation_router, prefix="/generate", tags=["Generation"])
 api_router.include_router(spaces_router, prefix="/spaces", tags=["Spaces"])
